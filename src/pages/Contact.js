b815a92255d0d59ec8056f437a5ea3da7fab1da1
Deep <deep@gmail.com>
2022-12-16 19:04:49 +0530

    styling changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,14 +2,15 @@ import React from "react";
 import ContactFrom from "../components/ContactFrom";
 export default function Contact() {
   return (
-    <>
+    <div>
       <div className="overlay"></div>
       <div className="top-block pink">
         <div className="contact-title top-title">contact</div>
         <div className="contact-head top-head">get in touch</div>
       </div>
-      <div className="contact-main-container"></div>
-      <ContactFrom />
-    </>
+      <div className="contact-main-container">
+        <ContactFrom />
+      </div>
+    </div>
   );
 }
